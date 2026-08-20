1875abe8b5894dcff3cd02b2a37d10f8a7b35c09
rishabh1sinha <119594774+rishabh1sinha@users.noreply.github.com>
2026-08-20 20:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>Dockerized Web Application</h1>
+    <p>My first DevOps internship project.</p>
+    <p>Application successfully deployed using Docker.</p>
+    """
+
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
